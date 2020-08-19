daf7193c184ed193600110b9aab700b9f1ffabdd
Aayush Vishnoi <56499156+aayush-vish@users.noreply.github.com>
2020-08-19 22:07:29 +0530

    Added the href property to icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,9 +7,16 @@ export default class Contact extends Component {
       <div className="box1">
         <h3>CONTACT</h3>
         <div className="icons">
-          <img src="https://img.icons8.com/material-sharp/32/000000/github.png" />
+          <a href="https://github.com/aayush-vish">
+          <img src="https://img.icons8.com/material-sharp/32/000000/github.png"/>
+          </a>
+          <a href="#">
           <img src="https://img.icons8.com/android/32/000000/twitter.png" />
+          </a>
+          <a href="https://www.linkedin.com/in/aayush-vishnoi-2b53b01b0">
           <img src="https://img.icons8.com/android/32/000000/linkedin.png" />
+          </a>
+          
           
         </div>
       </div>
